11d5d2f599e59440ea4dc7960757f95955c8bce2
ziwang0411 <ziwang0411@gmail.com>
2021-05-25 20:58:51 -0700

    finished testing

---
+++ src/test/java/com/example/demo/TestUtils.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import java.lang.reflect.Field;
+
+public class TestUtils {
+    public static void injectObjects(Object target, String fieldName, Object toInject) {
+        boolean wasPrivate = false;
+        Field f = null;
+        try {
+            f = target.getClass().getDeclaredField(fieldName);
+            if (!f.isAccessible()) {
+                f.setAccessible(true);
+                wasPrivate = true;
+            }
+            f.set(target, toInject);
+
+            if (wasPrivate) {
+                f.setAccessible(false);
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
